cb2111aa19a7a9bae5dcfa23515d2665a043d317
jahiel <jahiel@jahiel-LXM>
2013-11-05 18:30:00 +0100

    Añadido Main (codigo integracion)

--- src/practica/Launcher.java
+++ src/practica/Launcher.java
@@ -1,9 +1,37 @@
 package practica;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import practica.agent.*;
+import practica.util.ImgMapConverter;
+import practica.util.Map;
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.AgentsConnection;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DOMConfigurator.configure("src/thematrix/configuration/loggin.xml"); // ERR
+        Logger logger = Logger.getLogger(Launcher.class);
+        
+        // QPID
+        AgentsConnection.connect("localhost",5672, "test", "guest", "guest", false);
+        System.out.println("Main: Creando agentes");
+        
+        Map map = ImgMapConverter.imgToMap("src/Maps/map1.png");
+        AgentID id_satelite = new AgentID("Satelite");
+        
+        try{
+        	Satelite satelite = new Satelite(id_satelite, map);
+        	Drone drone = new Drone(new AgentID("Drone"), map.getWidth(), map.getHeigh(), id_satelite);
+        	System.out.println("MAIN : Iniciando agentes...");
+            satelite.start();
+            drone.start();
+        }catch(Exception e){
+        	System.err.println("Main: Error al crear los agentes");
+            System.exit(-1);
+        }
 
 	}
 
