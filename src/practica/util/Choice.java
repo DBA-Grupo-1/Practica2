f70d0371edb4410ad7a2325d462954caec9fdca3
jonay <jonaypl@ubuntu>
2013-12-15 16:09:48 +0100

    JavaDoc de las clases Choice, BadChoice, SharedSquare y SharedMap

--- src/practica/util/Choice.java
+++ src/practica/util/Choice.java
@@ -1,26 +1,63 @@
 package practica.util;
 
+/**
+ * Clase que permite almacenar la decisión tomada en un punto específicado mediante
+ * unas coordenadas.
+ * 
+ * @author jonay
+ */
 public class Choice {
 	private int move;
 	private GPSLocation location;
 	
+	/**
+	 * Constructor de Choice que recibe dos parámetros
+	 * @param move	La decisión sobre el movimiento que se ha tomado
+	 * @param loc	Las coordenadas a las que hace referencia esta decisión
+	 * 
+	 * @author jonay
+	 */
 	public Choice (int move, GPSLocation loc){
 		this.move = move;
 		this.location = loc;
 	}
 	
+	/**
+	 * Devuelve la decisión de movimiento
+	 * @return La decisión sobre el movimiento tomado.
+	 * 
+	 * @author jonay
+	 */
 	public int getMove(){
 		return move;
 	}
 	
+	/**
+	 * Asigna una decisión de movimiento a este Choice
+	 * @param move La decisión de movimiento
+	 * 
+	 * @author jonay
+	 */
 	public void setMove(int move){
 		this.move = move;
 	}
 	
+	/**
+	 * Devuelve las coordenadas donde se tomó la decisión 
+	 * @return Las coordenadas
+	 * 
+	 * @author jonay
+	 */
 	public GPSLocation getLocation(){
 		return location;
 	}
-	
+
+	/**
+	 * Asigna las coordenadas donde se tomó la decisión
+	 * @param location Las coordenadas
+	 * 
+	 * @author jonay
+	 */
 	public void setLocation(GPSLocation location){
 		this.location = location;
 	}
--- src/practica/util/SharedMap.java
+++ src/practica/util/SharedMap.java
@@ -1,22 +1,45 @@
 package practica.util;
 
 import java.util.ArrayList;
-
 import es.upv.dsic.gti_ia.core.AgentID;
 
+/**
+ * Esta clase crea un mapa que podrá guardar las casillas por donde ha pasado cada uno de los drones,
+ * así como marcar casillas con malas decisiones.
+ * 
+ * @author jonay
+ */
 public class SharedMap extends Map{
 	private SharedSquare sharedSquares[][];
 
+	/**
+	 * Constructor de SharedMap en el que se inicializan las casillas como vacías 
+	 * @param heigh	Altura del mapa
+	 * @param width Anchura del mapa
+	 * 
+	 * @author jonay
+	 */
 	public SharedMap(int heigh, int width) {
 		super(heigh, width);
 		initialize(heigh, width);
 	}
 
+	/**
+	 * Constructor por copia de un Map, se inicializarán los obstáculos y la meta
+	 * @param other	El mapa del que se obtendrán los datos
+	 */
 	public SharedMap(Map other) {
 		super(other);
 		initialize(other.getHeigh(),other.getWidth());
 	}
 	
+	/**
+	 * Inicializador de las casillas con SharedSquare vacíos
+	 * @param heigh	La altura del mapa 
+	 * @param width La anchura del mapa
+	 * 
+	 * @author jonay
+	 */
 	private void initialize(int heigh, int width){
 		sharedSquares = new SharedSquare[heigh][width];
 		
@@ -28,6 +51,12 @@ public class SharedMap extends Map{
 		}
 	}
 	
+	/**
+	 * Constructor por copia de otro SharedMap, se copiarán todos sus datos.
+	 * @param other El SharedMap del que copiar los datos
+	 * 
+	 * @author jonay
+	 */
 	public SharedMap(SharedMap other) {
 		super(other.getHeigh(), other.getWidth());
 		int width, heigh;
@@ -43,6 +72,18 @@ public class SharedMap extends Map{
 		}
 	}
 	
+	/**
+	 * Permite guardar información sobre una casilla, ya sea si hay un obstáculo
+	 * o ha sido visitada, guardando el drone que visitó la casilla.
+	 * @param x	Coordenada X de la casilla
+	 * @param y	Coordenada Y de la casilla
+	 * @param value	El valor a guardar (Constante estática de Map)
+	 * @param id Identificador del drone que quiere asignar el valor
+	 * @throws Exception Lanza una excepción si las coordenadas no se encuentran dentro
+	 * 			de los límites del mapa
+	 * 
+	 * @author jonay
+	 */
 	public void setValue(int x, int y, int value, AgentID id) throws Exception{
 		super.setValue(x, y, value);
 		if(value == VISITADO){
@@ -50,6 +91,13 @@ public class SharedMap extends Map{
 		}
 	}
 	
+	/**
+	 * @throws Exception Lanza una excepción si las coordenadas no se encuentran dentro
+	 * 			de los límites del mapa o si se quiere indicar como VISITADO una casilla,
+	 * 			pues se debe indicar el id del agente para las visitadas.
+	 * 
+	 * @author jonay
+	 */
 	@Override
 	public void setValue(int x, int y, int value) throws Exception {
 		if(value == VISITADO){
@@ -58,27 +106,70 @@ public class SharedMap extends Map{
 		super.setValue(x, y, value);
 	}
 
-	
+	/**
+	 * Devuelve los agentes que han visitado una coordenada específica
+	 * @param x	Coordenada X
+	 * @param y Coordenada Y
+	 * @return Array con los idenficadores de los agentes que han visitado la casilla
+	 * 
+	 * @author jonay
+	 */
 	public ArrayList<AgentID> getVisitingAgents(int x, int y){
 		return sharedSquares[y][x].getVisitingAgents();
 	}
 	
+	/**
+	 * Devuelve las indicaciones sobre malas decisiones tomadas en una casilla
+	 * @param x Coordenada X
+	 * @param y Coordenada Y
+	 * @return Array con las malas decisiones de la casilla
+	 * 
+	 * @author jonay
+	 */
 	public ArrayList<BadChoice> getBadChoices(int x, int y){
 		return sharedSquares[y][x].getBadChoices();
 	}
 	
+	/**
+	 * Devuelve el array con el contenido de las casillas de tipo SharedSquare
+	 * @return Array de SharedSquare
+	 * 
+	 * @author jonay
+	 */
 	public SharedSquare[][] getSharedSquares() {
 		return sharedSquares;
 	}
 	
+	/**
+	 * Añade un indicador de mala decisión a una casilla
+	 * @param x Coordenada X
+	 * @param y Coordenada Y
+	 * @param bc La mala elección
+	 * 
+	 * @author jonay
+	 */
 	public void addBadChoice(int x, int y, BadChoice bc){
 		sharedSquares[y][x].addBadChoice(bc);
 	}
 	
+	/**
+	 * Elimina las malas decisiones anotadas por un agente en una casilla
+	 * @param x Coordenada X
+	 * @param y Coordenada Y
+	 * @param id El id del agente
+	 * 
+	 * @author jonay
+	 */
 	public void removeBadChoice(int x, int y, AgentID id){
 		sharedSquares[y][x].removeBadChoice(id);
 	}
 	
+	/**
+	 * Devuelve true si existe alguna casilla con indicador de BadChoice, o false en caso contrario
+	 * @return Si tiene o no BadChoice en alguna casilla
+	 * 
+	 * @author jonay
+	 */
 	public boolean hasBadChoices(){
 		for(int x=0; x<this.getWidth(); x++){
 			for(int y=0; y<this.getHeigh(); y++){
