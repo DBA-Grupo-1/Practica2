98d91a29f56b1fef5035b9031be370190d06ee2f
Yarrick <potrayarrick@gmail.com>
2013-12-16 20:49:09 +0100

    Añadida biblioteca para guardar los nombres de los protocolos.

---
+++ src/practica/util/protocolLibrary.java
@@ -0,0 +1,8 @@
+package practica.util;
+
+public class protocolLibrary {
+
+	public static final String droneMovementProtocol = "I moved!";
+	public static final String sendMeMyStatusProtocol = "Send me my status";
+
+}
