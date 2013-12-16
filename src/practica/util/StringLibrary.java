a15fcfef250018403f9fb72fdcc06cbb638ceb42
Yarrick <potrayarrick@gmail.com>
2013-12-16 19:45:20 +0100

    Creada biblioteca con cadenas para ir metiendo las que nos vayan saliendo.

---
+++ src/practica/util/StringLibrary.java
@@ -0,0 +1,56 @@
+package practica.util;
+
+public class StringLibrary {
+	
+	//==========================================================================================================
+	//Cadenas de Charger
+	public static String reasonWontReachGoal = "You won't reach goal";
+	public static String reasonExplorerDidntReachGoal = "Current explorer didn't reach goal";
+	public static String reasonWaitingForBetterDrone = "I'm waiting for a better drone than you to reach goal";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	//==========================================================================================================
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	//==========================================================================================================
+
+	public StringLibrary() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
