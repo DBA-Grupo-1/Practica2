f3d9efed4937674a32bcb4fcfbbaade3e88589c7
Daniel <Daniel@192.168.1.130>
2013-12-29 21:11:37 +0100

    Creada biblioteca para keys JSON.

---
+++ src/practica/lib/JSONKeyLibrary.java
@@ -0,0 +1,25 @@
+package practica.lib;
+
+/**
+ * Biblioteca de Strings con las keys que usamos en los objetos JSON.
+ * @author Daniel
+ *
+ */
+public class JSONKeyLibrary {
+
+	/***************************************************************
+	 **********************GENERAL********************************** 
+	 **************************************************************/
+	public static final String Subject = "Subject";
+	public static final String DroneID = "DroneID";
+	
+	
+
+	/***************************************************************
+	 ****************ESPECÍFICOS DE PROTOCOLOS********************** 
+	 **************************************************************/
+	
+	//Recharge
+	public static final String RequestAmount = "Requested battery amount";
+	public static final String AmountGiven = "Given battery amount";
+}
