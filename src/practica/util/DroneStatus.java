54d06adacd944c8ce22b523f0442c0dba581b03c
Daniel <potrayarrick@gmail.com>
2014-01-14 20:07:18 +0100

    Comentada clase DroneStatus, añadido método toString.

--- src/practica/util/DroneStatus.java
+++ src/practica/util/DroneStatus.java
@@ -2,6 +2,12 @@ package practica.util;
 
 import es.upv.dsic.gti_ia.core.AgentID;
 
+
+/**
+ * Estado actual de un drone
+ * @author Daniel
+ *
+ */
 public class DroneStatus {
 
 	private int battery;
@@ -10,6 +16,13 @@ public class DroneStatus {
 	private boolean goalReached;
 	private GPSLocation location;
 	
+	/**
+	 * Constructor por defecto, pone la batería en 75.
+	 * @author Daniel
+	 * @param id Identificador del drone.
+	 * @param name Nombre del drone.
+	 * @param loc Posición en el mapa del drone.
+	 */
 	public DroneStatus(AgentID id, String name, GPSLocation loc) {
 		this.id = id;
 		this.name = name;
@@ -17,44 +30,102 @@ public class DroneStatus {
 		battery = 75;
 	}
 
+	/**
+	 * Devuelve la batería del drone.
+	 * @author Daniel
+	 * @return Batería del drone.
+	 */
 	public int getBattery() {
 		return battery;
 	}
 
+	/**
+	 * Cambia la batería del drone.
+	 * @author Daniel
+	 * @param battery cantidad nueva de batería.
+	 */
 	public void setBattery(int battery) {
 		this.battery = battery;
 	}
 
+	/**
+	 * Devuelve el identificador del drone.
+	 * @author Daniel
+	 * @return ID del drone.
+	 */
 	public AgentID getId() {
 		return id;
 	}
 
+	/**
+	 * Cambia el identificador del drone.
+	 * @author Daniel
+	 * @param id Nuevo ID del drone.
+	 */
 	public void setId(AgentID id) {
 		this.id = id;
 	}
 
+	/**
+	 * Devuelve el nombre del drone.
+	 * @author Daniel
+	 * @return Nombre del drone.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Cambia el nombre del drone.
+	 * @author Daniel
+	 * @param name Nuevo nombre del drone.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Comprueba si el drone ha llegado al objetivo.
+	 * @author Daniel
+	 * @return true si está en el objetivo, false si no.
+	 */
 	public boolean isGoalReached() {
 		return goalReached;
 	}
 
+	/**
+	 * Cambia el estado del drone respecto a si ha llegado o no al objetivo.
+	 * @author Daniel
+	 * @param goalReached true si ha llegado al objetivo, false si no.
+	 */
 	public void setGoalReached(boolean goalReached) {
 		this.goalReached = goalReached;
 	}
 
+	/**
+	 * Devuelve la posición en el mapa del drone.
+	 * @author Daniel
+	 * @return Posición GPS del drone.
+	 */
 	public GPSLocation getLocation() {
 		return location;
 	}
 
+	/**
+	 * Cambia la posición en el mapa del drone.
+	 * @author Daniel
+	 * @param location Nueva posición del drone.
+	 */
 	public void setLocation(GPSLocation location) {
 		this.location = location;
+	}
+	
+	/**
+	 * @author Daniel
+	 */
+	public String toString(){
+		return ("Pos: " + String.valueOf(location.getPositionX()) + "," + String.valueOf(location.getPositionY() + " Battery: "
+				+ String.valueOf(battery)));
 	}
 
 }
